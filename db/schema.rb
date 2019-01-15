bca51daf197cba07055a2fad2725c111de32991e
hiroki.okabe <hiroki12kainan19@outlook.jp>
2019-01-15 13:36:15 +0000

    bootstrap design

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190107143829) do
+ActiveRecord::Schema.define(version: 20190108073045) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
